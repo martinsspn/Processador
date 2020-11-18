d391b70d4e0e42c3ea224319931a58c53b23cec9
Gabriel Lucas <gabrielucasml@gmail.com>
2020-11-18 21:28:06 +0000

    implementação do pipeline

---
+++ com pipeline/buffer.cpp
@@ -0,0 +1,12 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <vector>
+
+using namespace std;
+
+class Buffer{
+	vector<int> buffer;
+};
+
+#endif
--- com pipeline/po.cpp
+++ com pipeline/po.cpp
@@ -11,6 +11,7 @@
 #include "regs.cpp"
 #include "multiplex.cpp"
 #include "add.cpp"
+#include "buffer.cpp"
 
 class Po{
 	private:
@@ -24,10 +25,11 @@ class Po{
 		Multiplex *m2;
 		Multiplex *m3;
 		Multiplex *m4;
+		Buffer *buffer;
 		Add *add;
 	public:	
 		Po(InstMem *_instMem, DataMem *_dataMem, Pc *_pc, ProgramCounter *_programCounter, Ula *_ula,
-		Regs *_regs, Multiplex *_m1, Multiplex *_m2, Multiplex *_m3, Multiplex *_m4,Add *_add){
+		Regs *_regs, Multiplex *_m1, Multiplex *_m2, Multiplex *_m3, Multiplex *_m4,Add *_add, Buffer *buffer){
 			instMem = _instMem;
 			dataMem = _dataMem;
 			pc = _pc;
@@ -39,15 +41,30 @@ class Po{
 			m3 = _m3;
 			m4 = _m4;
 			add = _add;
+
 		}
 
 		void executar(){
-			int x, y, z, k, j, w, h;
 			int operationType;
 			operationType = pc->decodificar(instMem->rm(programCounter->get(), true));
 			for(int i=0; i<instMem->memoria.size(); i++){
 				switch(operationType){
 					case 1: //sum, sub, div, mult, and, or, beq
+						if(pc->opCode == "SUM"){
+							buffer->buffer[i] = ula->op(1, regs->registradores[pc->opFont1], regs->registradores[pc->opFont2], regs->registradores[pc->dataAddress]);
+						}else if(pc->opCode == "SUB"){
+
+						}else if(pc->opCode == "DIV"){
+
+						}else if(pc->opCode == "MULT"){
+
+						}else if(pc->opCode == "AND"){
+
+						}else if(pc->opCode == "OR"){
+
+						}else if(pc->opCode == "BEQ"){
+
+						}
 						break;
 					case 2: //not, load, store
 						break;
@@ -56,6 +73,8 @@ class Po{
 					default:
 						break;
 				}
+				programCounter->set(i, true);
+				operationType = pc->decodificar(instMem->rm(programCounter->get(), true));
 				
 			}
 		}	
